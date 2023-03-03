ba180fac77d8d49450d74bd7c54a28c64a660162
imhotkp <singhkaushlendra1990@gmail.com>
2023-03-03 21:43:10 +0530

    first commit

---
+++ js/durgesh.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+   
+   $(".menus li a").click(function(e){
+    let target= $(this).attr("href");
+    $("html,body").animate(
+        {
+            scrollTop:$(target).offset().top-40,
+        },1000
+    );
+    e.preventDefault();
+});
+});
